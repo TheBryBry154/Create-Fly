6123f9b215a3ef8ac53bbdea5c6f4872eac473ec
BuildTools <unconfigured@null.spigotmc.org>
2024-10-06 18:31:55 -0400

    a

---
+++ createflyfabric/src/main/java/evy/evy/createflyfabric/Createflyfabric.java
@@ -0,0 +1,10 @@
+package evy.evy.createflyfabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Createflyfabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
